dbb5fbd5fea6505024b6924f6c471856d13fb143
kamalcharan <kamalcharan@yahoo.com>
2025-07-22 19:40:27 +0530

    masterDataRoutes correction for deployment

--- src/routes/masterDataRoutes.ts
+++ src/routes/masterDataRoutes.ts
@@ -1,17 +1,20 @@
-// src/routes/masterDataRoutes.ts
 import express from 'express';
-import * as masterDataController from '../controllers/masterDataController';
+import { 
+  getCategories,
+  getCategoryDetails,
+  getNextSequenceNumber,
+  addCategoryDetail,
+  updateCategoryDetail,
+  deleteCategoryDetail
+} from '../controllers/masterDataController';
 
 const router = express.Router();
 
-// Category routes
-router.get('/categories', masterDataController.getCategories);
-
-// Category Details routes
-router.get('/category-details', masterDataController.getCategoryDetails);
-router.get('/next-sequence', masterDataController.getNextSequenceNumber);
-router.post('/category-details', masterDataController.addCategoryDetail);
-router.patch('/category-details/:id', masterDataController.updateCategoryDetail);
-router.delete('/category-details/:id', masterDataController.deleteCategoryDetail);
+router.get('/categories', getCategories);
+router.get('/category-details', getCategoryDetails);
+router.get('/next-sequence', getNextSequenceNumber);
+router.post('/category-details', addCategoryDetail);
+router.patch('/category-details/:id', updateCategoryDetail);
+router.delete('/category-details/:id', deleteCategoryDetail);
 
 export default router;
