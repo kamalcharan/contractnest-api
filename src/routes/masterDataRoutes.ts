ca3275728bf97b31314c751d3caceb728f999276
kamalcharan <kamalcharan@yahoo.com>
2025-07-22 20:54:40 +0530

    masterDataRoutes fixed for railway runtime error

--- src/routes/masterDataRoutes.ts
+++ src/routes/masterDataRoutes.ts
@@ -1,20 +1,13 @@
 import express from 'express';
-import { 
-  getCategories,
-  getCategoryDetails,
-  getNextSequenceNumber,
-  addCategoryDetail,
-  updateCategoryDetail,
-  deleteCategoryDetail
-} from '../controllers/masterDataController';
+import * as masterDataController from '../controllers/masterDataController';
 
 const router = express.Router();
 
-router.get('/categories', getCategories);
-router.get('/category-details', getCategoryDetails);
-router.get('/next-sequence', getNextSequenceNumber);
-router.post('/category-details', addCategoryDetail);
-router.patch('/category-details/:id', updateCategoryDetail);
-router.delete('/category-details/:id', deleteCategoryDetail);
+router.get('/categories', masterDataController.getCategories);
+router.get('/category-details', masterDataController.getCategoryDetails);
+router.get('/next-sequence', masterDataController.getNextSequenceNumber);
+router.post('/category-details', masterDataController.addCategoryDetail);
+router.patch('/category-details/:id', masterDataController.updateCategoryDetail);
+router.delete('/category-details/:id', masterDataController.deleteCategoryDetail);
 
 export default router;
